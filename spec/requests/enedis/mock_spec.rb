ea9a2f869b7d4917ac47ca3354c442b9645e2ba9
Noah Delpit <noah02delpit@gmail.com>
2025-03-25 17:18:07 +0100

    Ajout des tests d'intégration pour les contrôleurs Enedis

---
+++ spec/requests/enedis/mock_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+
+RSpec.describe "Enedis::Mocks", type: :request do
+  describe "GET /index" do
+  end
+end
---
+++ spec/views/enedis/consumptions/show.html.tailwindcss_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe "consumptions/show.html.tailwindcss", type: :view do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
